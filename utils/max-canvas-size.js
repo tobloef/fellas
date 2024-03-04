39f001fd66aac9c7d31d5bd7c87bd4d877b9cd1f
Tobias Løfgren <tobloef@gmail.com>
2024-03-04 09:47:43 +0100

    Implement `findMaxCanvasSize()`

---
+++ utils/max-canvas-size.js
@@ -0,0 +1,82 @@
+// See this library for a more complete solution
+// https://github.com/jhildenbiddle/canvas-size/
+
+const FILL_COLOR = [255, 0, 0, 255];
+
+export function findMaxCanvasSize() {
+  const offscreenCanvas = new OffscreenCanvas(1, 1);
+  const ctx = offscreenCanvas.getContext('2d', { willReadFrequently: true });
+  ctx.fillStyle = `rgba(${FILL_COLOR.join(',')})`;
+
+  let size = 1;
+  let lowerBound = 1;
+  let upperBound = null;
+
+  ({ size, lowerBound, upperBound } = powerOfTwoSearch(ctx, size, lowerBound, upperBound));
+  ({ size } = binarySearch(ctx, size, lowerBound, upperBound));
+
+  return size;
+}
+
+function powerOfTwoSearch(ctx, size, lowerBound, upperBound) {
+  while (true) {
+    let newSize = size * 2;
+    const supportsSize = checkSize(ctx, newSize);
+    if (supportsSize) {
+      size = newSize;
+      lowerBound = newSize;
+    } else {
+      upperBound = newSize;
+      newSize = lowerBound + 1;
+      const supportsNewSize = checkSize(ctx, newSize);
+      if (supportsNewSize) {
+        size = newSize;
+        lowerBound = newSize;
+      } else {
+        upperBound = newSize;
+      }
+      break;
+    }
+  }
+
+  return { size, lowerBound, upperBound };
+}
+
+function binarySearch(ctx, size, lowerBound, upperBound) {
+  while (true) {
+    if (size === lowerBound) {
+      break;
+    }
+
+    const supportsSize = checkSize(ctx, size);
+
+    if (supportsSize) {
+      lowerBound = size;
+    } else {
+      upperBound = size;
+    }
+
+    if (upperBound === null) {
+      size *= 2;
+    } else {
+      size = Math.floor((upperBound - lowerBound) / 2) + lowerBound;
+    }
+  }
+
+  return { size, lowerBound, upperBound };
+}
+
+function checkSize(ctx, size) {
+  ctx.canvas.width = size;
+  ctx.canvas.height = size;
+
+  ctx.fillRect(size - 1, size - 1, 1, 1);
+  const data = ctx.getImageData(size - 1, size - 1, 1, 1).data;
+
+  return (
+    data[0] === FILL_COLOR[0] &&
+    data[1] === FILL_COLOR[1] &&
+    data[2] === FILL_COLOR[2] &&
+    data[3] === FILL_COLOR[3]
+  );
+}
